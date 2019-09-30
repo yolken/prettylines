11da79021a59f418492be539c54b6feadb9f8804
Benjamin Yolken <yolken@gmail.com>
2019-09-29 22:35:38 -0700

    More formatting updates

--- _testfiles/test.go
+++ _testfiles/test.go
@@ -18,6 +18,8 @@ func longLine(aReallyLongName string, anotherLongName string, aThirdLongName str
 
 	fmt.Printf("This is a really long line that can be broken up twice %s %s", fmt.Sprintf("This is a really long sub-line that should be broken up more because %s %s", argument1, argument2), fmt.Sprintf("A short one %d", 3))
 
+	fmt.Print("This is a function with a really long single argument. We want to see if it's properly split")
+
 	fmt.Println(z)
 
 	return "", nil
